60ccdf5fa70e29807c01ba458c0d876dcd6473d8
stefan22 <stefanosemail@protonmail.com>
2021-09-22 19:28:46 +0100

    create pages

---
+++ src/pages/jackets/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+const Jackets = () => (
+  <div className="home-page">
+    <div className="home-page--wrapper">
+      <h1>Jackets</h1>
+    </div>
+  </div>
+);
+
+export default Jackets;
---
+++ src/pages/men/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+const Men = () => (
+  <div className="home-page">
+    <div className="home-page--wrapper">
+      <h1>Men</h1>
+    </div>
+  </div>
+);
+
+export default Men;
