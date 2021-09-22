5e210a8b257bcd633483190fb986e93c3101532a
stefan22 <stefanosemail@protonmail.com>
2021-09-23 00:31:49 +0100

    comps housekeep

--- src/components/menu-item/index.js
+++ src/components/menu-item/index.js
@@ -1,20 +1,31 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
-import "./styles.scss";
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import './styles.scss';
 
-const MenuItem = ({title, imageUrl, size, linkUrl, history, match}) => (
+const MenuItem = ({
+  title,
+  imageUrl,
+  size,
+  linkUrl,
+  history,
+  match,
+}) => (
   <div
     onClick={() => history.push(`${match.url}${linkUrl}`)}
-    style={{ 
-      backgroundImage: `url(${imageUrl})`, 
-      backgroundSize:'cover', 
+    style={{
+      backgroundImage: `url(${imageUrl})`,
+      backgroundSize: 'cover',
       backgroundPosition: 'center',
-  }}
-    className={`directory-menu__item ${!size ? 'regular' : size}`}
+    }}
+    className={`directory-menu__item ${
+      !size ? 'regular' : size
+    }`}
   >
     <div className="content-item">
       <h1 className="content-item__title">{title}</h1>
-      <span className="content-item__subtitle">SHOP NOW</span>
+      <span className="content-item__subtitle">
+        SHOP NOW
+      </span>
     </div>
   </div>
 );
