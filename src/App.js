72fd9ffa8ebb66bc0c8f517b153113a785657523
Stefano L <stefanosemail@protonmail.com>
2022-08-10 02:28:16 +0100

    Remove unused selector

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { Routes, Route, Outlet } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { setCurrentUser } from './redux-store/user/userActions'
 import {
   createUserDocumentFromAuth,
@@ -23,7 +23,6 @@ import * as ROUTES from './helpers/constants/routes'
 
 const App = () => {
   const dispatch = useDispatch()
-  const categories = useSelector(state => state.categories)
 
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener(user => {
