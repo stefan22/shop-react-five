05bc959812bd41a07da48151f6f2f45514ae740e
stefan22 <stefanosemail@protonmail.com>
2022-08-13 18:04:49 +0100

    redux persist storage

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './redux-store/store'
+import { store, persistor } from './redux-store/store'
+import { PersistGate } from 'redux-persist/integration/react'
 import App from './App'
 //styles
 import './styles/main.scss'
@@ -14,11 +15,16 @@ store.subscribe(() => {
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <div className="main-wrapper">
-        <App />
-      </div>
-    </BrowserRouter>
+    <PersistGate
+      loading={null}
+      persistor={persistor}
+    >
+      <BrowserRouter>
+        <div className="main-wrapper">
+          <App />
+        </div>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
 
   document.getElementById('root')
